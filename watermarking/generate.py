e807b70794f9a3dd09c1c25996edea983db5aa29
Minh <blueminh789.coding@gmail.com>
2024-05-06 17:37:29 +0200

    samples with watermark applied

--- watermarking/generate.py
+++ watermarking/generate.py
@@ -1,7 +1,7 @@
 from be_great import GReaT
 import transformers
-from lm_watermarking import extended_watermark_processor
 
+from lm_watermarking.extended_watermark_processor import WatermarkLogitsProcessor
 
 model_path = "/Users/minhkau/Documents/TUDelft/Year 3/RP/Code/tabular-gpt/be_greater/models/adult_v_baseline"
 
@@ -12,11 +12,11 @@ great.tokenizer.pad_token_id = great.tokenizer.eos_token_id
 great.tokenizer.pad_token = great.tokenizer.eos_token
 
 # set up watermark
-watermark_processor = extended_watermark_processor.WatermarkLogitsProcessor(vocab=list(great.tokenizer.get_vocab().values()),
+watermark_processor = WatermarkLogitsProcessor(vocab=list(great.tokenizer.get_vocab().values()),
                                                gamma=0.25,
                                                delta=2.0,
                                                seeding_scheme="selfhash")
 
 # generation
-samples = great.sample(10, k=200, max_length=400, device="cpu", logits_processor=watermark_processor)
-samples.to_csv("test_sample.csv", index=False)
+samples = great.sample(100, k=10, max_length=400, device="cpu", logits_processor=watermark_processor)
+samples.to_csv("watermaked_adult_samples_100.csv", index=False)
