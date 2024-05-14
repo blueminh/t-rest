01ad67636d9963ccd4abcb6b8811af319e657e95
Minh <blueminh789.coding@gmail.com>
2024-05-14 10:17:07 +0200

    naming generated samples

--- watermarking/apply_watermark.py
+++ watermarking/apply_watermark.py
@@ -27,36 +27,54 @@ def generation(model_name, sample_size, batch_size=50, write_to_file=False, with
                                                                  delta=delta)
         samples = great.sample(sample_size, k=batch_size, max_length=400, device="cpu",
                                logits_processor=great_watermark_processor)
-        sample_file_name = "{}_{}_{}.csv".format(model_name, sample_size, "with-watermark")
+        sample_file_name = "{}_{}_{}_gamma-{}_delta-{}.csv".format(model_name, sample_size, "with-watermark", gamma,
+                                                                   delta)
     else:
         samples = great.sample(sample_size, k=batch_size, max_length=400, device="cpu")
-        sample_file_name = "{}_{}_{}_gamma-{}_delta-{}.csv".format(model_name, sample_size, "non-watermark", gamma,
-                                                                   delta)
+        sample_file_name = "{}_{}_{}.csv".format(model_name, sample_size, "non-watermark")
 
     if write_to_file:
-        samples.to_csv(samples_dir + sample_file_name)
+        samples.to_csv(samples_dir + sample_file_name, index=False)
+
+    return samples
 
 
-def detection(sample_name, model_name, gamma, delta, included_columns=None):
+def detection(sample_name, gamma, delta, included_columns=None, sample_df=None, print_tokens=False):
     samples_dir = "/Users/minhkau/Documents/TUDelft/Year 3/RP/Code/tabular-gpt/samples/"
-    great = load_model(model_name)
-    great_watermark_processor = GreatWatermarkLogitProcessor(tokenizer=great.tokenizer,
+    tokenizer = transformers.AutoTokenizer.from_pretrained('distilgpt2', add_prefix_space=True)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.pad_token = tokenizer.eos_token
+    great_watermark_processor = GreatWatermarkLogitProcessor(tokenizer=tokenizer,
                                                              device="cpu",
-                                                             vocab=list(great.tokenizer.get_vocab().values()),
+                                                             vocab=list(tokenizer.get_vocab().values()),
                                                              gamma=gamma,
                                                              delta=delta)
     great_watermark_detector = GreatWatermarkDetector(great_watermark_processor)
+    data = sample_df if sample_df else pd.read_csv(samples_dir + sample_name)
+    if print_tokens:
+        great_watermark_detector.print_with_color(data)
 
-    data = pd.read_csv(samples_dir + sample_name)
-    final_scores = great_watermark_detector.detect(data, included_columns)
-
-    pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(final_scores)
+    return great_watermark_detector.detect(data, included_columns)
 
 
-detection("watermarked_180.csv",
-          model_name="adult_v_baseline",
-          gamma=0.24, delta=2.0,
-          included_columns=["fnlwgt"])
+# generation(
+#     model_name="california",
+#     sample_size=100,
+#     batch_size=10,
+#     write_to_file=True,
+#     with_watermark=True,
+#     gamma=0.25,
+#     delta=2.0
+# )
 
-# great_watermark_detector.detect(real_data)
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(detection("california_100_non-watermark.csv",
+                    gamma=0.25,
+                    delta=2.0,
+                    print_tokens=True)['z_score'])
+
+pp.pprint(detection("california_100_with-watermark_gamma-0.25_delta-2.0.csv",
+                    gamma=0.25,
+                    delta=2.0,
+                    print_tokens=True)['z_score']
+          )
