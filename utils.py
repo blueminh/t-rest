081b2f3fc11d6f5120383d2794e87cde25fbc11c
Minh <blueminh789.coding@gmail.com>
2024-06-02 14:16:22 +0200

    added exp

--- utils.py
+++ utils.py
@@ -13,3 +13,13 @@ def create_file_name(model_name, sample_size, is_watermarked, gamma=None, delta=
             delta
         )
     return "{}_{}_{}.csv".format(model_name, sample_size, "non-watermark")
+
+
+def create_file_name_exp(model_name, sample_size, is_watermarked):
+    if is_watermarked:
+        return "{}_{}_{}_exp.csv".format(
+            model_name,
+            sample_size,
+            "with-watermark",
+        )
+    return "{}_{}_{}_exp.csv".format(model_name, sample_size, "non-watermark")
